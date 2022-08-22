f8f34fc7e2db29d48b9105d8a17b198a067ece5b
BSEF19M504 <bsef19m504@pucit.edu.pk>
2022-08-22 12:01:08 +0500

    Initial commit

---
+++ app/src/main/java/com/example/quran/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.quran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DataObject dataObject = new DataObject();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,dataObject.GetSurahNames());
+        ListView listView = findViewById(R.id.listSurah);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                
+            }
+        });
+    }
+}
