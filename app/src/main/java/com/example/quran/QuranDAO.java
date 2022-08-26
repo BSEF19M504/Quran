c5fc6e1e05306d989c0bea9d18f7ab2e2158dda6
BSEF19M504 <bsef19m504@pucit.edu.pk>
2022-08-26 23:10:58 +0500

    Made changes to database access functionality

--- app/src/main/java/com/example/quran/QuranDAO.java
+++ app/src/main/java/com/example/quran/QuranDAO.java
@@ -1,23 +1,23 @@
 package com.example.quran;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class QuranDAO extends SQLiteOpenHelper {
-    public static final String STUDENT_ID = "StudentID";
-    public static final String STUDENT_NAME = "StudentName";
-    public static final String STUDENT_ROLL = "StudentRollNumber";
-    public static final String STUDENT_ENROLL = "IsEnrolled";
-    public static final String STUDENT_TABLE = "StudentTable";
-
     public static final String SURAH_TABLE = "tsurah";
+    public static final String PARAH_TABLE = "tparah";
     public static final String AYAT_TABLE = "tayah";
     public static final String AYAT_ID = "AyaID";
     public static final String PARAH_ID = "ParaID";
@@ -25,18 +25,109 @@ public class QuranDAO extends SQLiteOpenHelper {
     public static final String ARABIC = "ArabicText";
     public static final String [] TRANSLATE = {"FatehMuhammadJalandhri","MehmoodulHassan","DrMohsinKhan","MuftiTaqiUsmani"};
 
+    //The Android's default system path of your application database.
+    private static final String DB_PATH = "/data/data/com.example.quran/databases/";
+    // Data Base Name.
+    private static final String DATABASE_NAME = "Quran.db";
+    // Data Base Version.
+    private static final int DATABASE_VERSION = 1;
+    // Table Names of Data Base.
+    static final String TABLE_Name = "tableName";
 
-    public QuranDAO(@Nullable Context context) {
-        super(context, "Quran.db", null, 1);
+    public Context context;
+    static SQLiteDatabase sqliteDataBase;
+
+    /**
+     * Constructor
+     * Takes and keeps a reference of the passed context in order to access to the application assets and resources.
+     * @param context
+     * Parameters of super() are    1. Context
+     *                              2. Data Base Name.
+     *                              3. Cursor Factory.
+     *                              4. Data Base Version.
+     */
+    public QuranDAO(Context context) {
+        super(context, DATABASE_NAME, null ,DATABASE_VERSION);
+        this.context = context;
+    }
+
+    /**
+     * Creates a empty database on the system and rewrites it with your own database.
+     * By calling this method and empty database will be created into the default system path
+     * of your application so we are gonna be able to overwrite that database with our database.
+     * */
+    public void createDataBase() throws IOException{
+        //check if the database exists
+        boolean databaseExist = checkDataBase();
+
+        if(!databaseExist){
+            this.getWritableDatabase();
+            copyDataBase();
+        }// end if else dbExist
+    } // end createDataBase().
+
+    /**
+     * Check if the database already exist to avoid re-copying the file each time you open the application.
+     * @return true if it exists, false if it doesn't
+     */
+    public boolean checkDataBase(){
+        File databaseFile = new File(DB_PATH + DATABASE_NAME);
+        return databaseFile.exists();
+    }
+
+    /**
+     * Copies your database from your local assets-folder to the just created empty database in the
+     * system folder, from where it can be accessed and handled.
+     * This is done by transferring byte stream.
+     * */
+    private void copyDataBase() throws IOException{
+        //Open your local db as the input stream
+        InputStream myInput = context.getAssets().open(DATABASE_NAME);
+        // Path to the just created empty db
+        String outFileName = DB_PATH + DATABASE_NAME;
+        //Open the empty db as the output stream
+        OutputStream myOutput = new FileOutputStream(outFileName);
+        //transfer bytes from the input file to the output file
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer))>0){
+            myOutput.write(buffer, 0, length);
+        }
+
+        //Close the streams
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+
+    /**
+     * This method opens the data base connection.
+     * First it create the path up till data base of the device.
+     * Then create connection with data base.
+     */
+    public void openDataBase() throws SQLException{
+        //Open the database
+        String myPath = DB_PATH + DATABASE_NAME;
+        sqliteDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+    }
+
+    /**
+     * This Method is used to close the data base connection.
+     */
+    @Override
+    public synchronized void close() {
+        if(sqliteDataBase != null)
+            sqliteDataBase.close();
+        super.close();
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
 
     }
 
