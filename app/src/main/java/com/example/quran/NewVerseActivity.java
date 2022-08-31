2f83154ba641854e0395629b11d2090244210756
BSEF19M504 <bsef19m504@pucit.edu.pk>
2022-08-31 21:47:07 +0500

    Added NewVerseActivity for displaying verses of The Quran

---
+++ app/src/main/java/com/example/quran/NewVerseActivity.java
@@ -0,0 +1,18 @@
+package com.example.quran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NewVerseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_verse);
+    }
+}
