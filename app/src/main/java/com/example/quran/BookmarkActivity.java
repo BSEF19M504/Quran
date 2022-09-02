d78cf3ce6ab96e695704357e0370ee301951fd0c
BSEF19M504 <bsef19m504@pucit.edu.pk>
2022-09-02 10:09:52 +0500

    Fixed Error with ListView BookmarkActivity onItemClickListener event

--- app/src/main/java/com/example/quran/BookmarkActivity.java
+++ app/src/main/java/com/example/quran/BookmarkActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +19,19 @@ public class BookmarkActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        Intent intent = getIntent();
+        int translateEng = intent.getIntExtra("translateEng",0);
+        int translateUrdu = intent.getIntExtra("translateUrdu",0);
+
+        Intent newIntent = new Intent(BookmarkActivity.this, VerseActivity.class);
+        newIntent.putExtra("translateEng", translateEng);
+        newIntent.putExtra("translateUrdu", translateUrdu);
         ArrayList<Ayat> ayatArrayList = new QuranDAO(getApplicationContext()).getAyatByBookmark();
         if(ayatArrayList.isEmpty()){
             listView.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1,new String[] {"No bookmarks saved!"}));
         }
         else{
-            listView.setAdapter(new BookmarkAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,ayatArrayList));
+            listView.setAdapter(new BookmarkAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,ayatArrayList, newIntent));
         }
     }
 
@@ -39,15 +47,20 @@ public class BookmarkActivity extends AppCompatActivity {
         List<Ayat> array;
         array = quranDAO.getAyatByBookmark();
 
+        Intent newIntent = new Intent(BookmarkActivity.this, VerseActivity.class);
+        newIntent.putExtra("translateEng", translateEng);
+        newIntent.putExtra("translateUrdu", translateUrdu);
+
         listView = findViewById(R.id.listBookmarks);
-        bookmarkAdapter = new BookmarkAdapter(BookmarkActivity.this, android.R.layout.simple_list_item_1,array);
+        bookmarkAdapter = new BookmarkAdapter(BookmarkActivity.this, android.R.layout.simple_list_item_1,array, newIntent);
         listView.setAdapter(bookmarkAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        /*listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Ayat ayat = (Ayat) adapterView.getItemAtPosition(i);
                 int id = ayat.getSurahId();
+                Toast.makeText(BookmarkActivity.this, Integer.toString(i), Toast.LENGTH_SHORT).show();
                 SurahNames surah = quranDAO.getSurahById(id);
 
                 Intent intent = new Intent(BookmarkActivity.this, VerseActivity.class);
@@ -63,7 +76,7 @@ public class BookmarkActivity extends AppCompatActivity {
                     intent.putExtra("StaringIndex", ayat.getAyatNo());
                 startActivity(intent);
             }
-        });
+        });*/
 
     }
 }
